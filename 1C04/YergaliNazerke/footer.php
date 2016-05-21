6add7e68ba1fb98a45b8e1bc5fc145a0027f6a8a
NazerkeYergali <nazerke.yergali@ce.sdu.edu.kz>
2016-05-21 16:35:20 +0600

    Create footer.php

---
+++ 1C04/YergaliNazerke/footer.php
@@ -0,0 +1,20 @@
+<div class="footer-right">
+                <a href="https://www.facebook.com"><img src="images/Follow_FB.png"/></i></a>
+                <a href="https://twitter.com"><img src="images/Follow_Twitter.png"/></a>
+                <a href="http://www.youtube.com"><img src="images/Follow_YT.png"/></a>
+                <a href="http://pinterest.com"><img src="images/Follow_Pin.png"/</a>
+            </div>
+            <div class="footer-left">
+                <p class="footer-links">
+                    <a href="index.php">Home</a>
+                    ·
+                    <a href="register.php">Register</a>
+                    ·
+                    <a href="all.php">All books</a>
+                    ·
+                    <a href="#">About</a>
+                    ·
+                    <a href="myaccount.php">Account</a>
+                </p>
+                <p>LIBRARY &copy; 2016</p>
+            </div>
