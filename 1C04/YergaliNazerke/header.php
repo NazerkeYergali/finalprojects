5f813dd85f71f39ad5eaa6ce3f53efc032282ebe
NazerkeYergali <nazerke.yergali@ce.sdu.edu.kz>
2016-05-21 16:36:07 +0600

    Create header.php

---
+++ 1C04/YergaliNazerke/header.php
@@ -0,0 +1,22 @@
+			  <div class="logo"><a href="index.php"><h2>LIBRARY</h2></a></div>
+        <div id="menu">
+            <ul>                                                                       
+            <li><a href="index.php">Home</a></li>
+            <li><a href="">About</a></li>
+            <li class="dropdown"><a href="#">Books</a>
+            <div class="dropdown-content">
+    			<a href="drama.php">Drama</a>
+    			<a href="fantasy.php">Fantasy</a>
+    			<a href="mystery.php">Mystery</a>
+  				  </div></li>
+            <li><a href="all.php">All books</a></li>
+            <li><a href="register.php">Register</a></li>
+            <li><a href="myaccount.php">Account</a></li>
+            </ul>
+        </div> 
+        <div id="sb">
+          <form action="/" method="get" >
+              Title: <input name="title" type="text" value="" class="text" style="width:70px"/> &nbsp;
+              <input type="submit" value="Go" class="submit" />
+          </form>
+      </div>
